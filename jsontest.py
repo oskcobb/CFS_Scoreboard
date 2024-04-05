c235021d363cad5521c956a3201713feb7063ae6
Oskar Cobb <cobboskar9@gmail.com>
2024-04-05 11:04:35 -0500

    updated and fixed a few things

--- jsontest.py
+++ jsontest.py
@@ -1,8 +1,22 @@
 import json
 import requests
 
+name = "Vollyball"
+
 r = requests.get('https://raw.githubusercontent.com/oskcobb/CFS_Scoreboard-Themes/main/themes.json')
-j = json.loads(r.text)
-for num in j["themes"]:
-    num = num["name"]
-    print(num)
+themes = json.loads(r.text)
+for theme in themes["themes"]:
+    print(theme)
+    print(name)
+    print()
+    if theme["name"] == name:
+        temp = theme["name"]
+        url = theme["url"]
+        print(url)
+        try:
+            print(url)
+            dl = requests.get(url).content
+        except:
+            print("Error")
+            pass
+        print(dl)
